84b5f4e0abeff3fda3ce7e4d8f63cc1f04bbb0e1
ChanderG <chandergovind@gmail.com>
2014-07-03 19:24:53 +0530

    Added base qunit test

---
+++ tests/testrunner.js
@@ -0,0 +1,5 @@
+var runner = require("/home/chander/Documents/Projects/ProjectFile/learnings/Js-file/node_modules/qunit");
+runner.run({
+    code : "/home/chander/Documents/Projects/ProjectFile/learnings/Js-file/hello.js",
+    tests : "/home/chander/Documents/Projects/ProjectFile/learnings/Js-file/tests/tests.js"
+});
